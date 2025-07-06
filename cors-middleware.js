bebe41bea249ba46fca621936b082ab5725d0475
Your Name <your.email@example.com>
2025-07-06 01:12:03 -0400

    Fix CORS to allow https://www.mountainshares.us

---
+++ cors-middleware.js
@@ -0,0 +1,18 @@
+// cors-middleware.js
+const allowedOrigins = [
+  'https://www.mountainshares.us'
+];
+
+module.exports = function(req, res, next) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+  }
+  next();
+};
